6ca6f2dee2b21c36b29f3c18ee43cd1b56ad7752
Maxime Pelletier <pelletiermaxime@gmail.com>
2014-08-24 17:55:30 -0400

    Initial plugin. Works fine, but need to customize documentation and options before publishing it.

---
+++ linter.py
@@ -0,0 +1,37 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by
+# Copyright (c) 2014
+#
+# License: MIT
+#
+
+"""This module exports the Foodcritic plugin class."""
+
+# from SublimeLinter.lint import RubyLinter, util
+from SublimeLinter.lint import Linter
+
+# class Foodcritic(RubyLinter):
+class Foodcritic(Linter):
+
+    """Provides an interface to foodcritic."""
+
+    syntax = 'ruby'
+    cmd = 'foodcritic @'
+    executable = None
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 1.0'
+    regex = r'(?P<message>FC\d+: .+): (?P<file>.+):(?P<line>.+)'
+    multiline = False
+    line_col_base = (1, 1)
+    tempfile_suffix = None
+    error_stream = util.STREAM_BOTH
+    selectors = {}
+    word_re = None
+    defaults = {}
+    inline_settings = None
+    inline_overrides = None
+    comment_re = r'\s*#'
